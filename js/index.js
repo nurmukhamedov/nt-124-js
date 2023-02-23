fdaebdfb765223fab04a21b00f0f67cf825e066e
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-02-23 09:36:13 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+// var firstName = 'Muhammadrasul';
+// var age = 22;
+
+// console.log(typeof firstName);
+// console.log(typeof age);
+
+// var studentId = "123465";
+
+// console.log(typeof studentId);
+
+// var isStudent = true;
+// var isMarried = false;
+
+// console.log(typeof isStudent);
+// console.log(typeof isMarried);
+
+
+// var myAge;
+
+// console.log(typeof myAge);
+// var myAge = 22;
+// console.log(myAge);
+
+// var education = null;
+// console.log(education);
+
+// var id = Symbol('5');
+
+// var hugeNumber = BigInt(99566513516846521313213210);
