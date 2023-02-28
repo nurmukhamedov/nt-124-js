fc60318fc3aae0a54b9e7f5ca3b8a7da80475426
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-02-28 10:29:59 +0500

    lesson-28

--- js/index.js
+++ js/index.js
@@ -1,30 +1,145 @@
+// let century = 21;
 
-// var firstName = 'Muhammadrasul';
-// var age = 22;
+// const userBirthYear = +prompt('Yilingizni kiriting')
+// if (userBirthYear <= 2000) {
+//     console.log(`Siz ${century - 1} asr odamisiz`);
+// } else {
+//     console.log(`Si ${century} odamisiz`);
+// }
 
-// console.log(typeof firstName);
-// console.log(typeof age);
+// task2
 
-// var studentId = "123465";
+// const num1 = +prompt('Birinchi son');
+// const num2 = +prompt('Ikkinchi son');
+// const total = +prompt('Birinchi va ikkinchi son yigindisi');
 
-// console.log(typeof studentId);
+// if (total === num1 + num2) {
+//     console.log(`Javbingiz to'gri haqiqatdan ${num1 + num2} edi`);
+// } else {
+//     console.log(`Javobingiz xato ${num1 + num2} bo'lishi kerak edi`);
+// }
 
-// var isStudent = true;
-// var isMarried = false;
+// task3
 
-// console.log(typeof isStudent);
-// console.log(typeof isMarried);
+// let hotelPrice = 250;
+// let flightPrice = 500;
+// let entertainmentPrice = 120;
+// let dollarToUzs = 11000;
+// let euroToUzs = 12000;
+
+// const totalPriceInUzs = (hotelPrice + flightPrice) * dollarToUzs + (entertainmentPrice * euroToUzs);
+
+// const userMoney = +prompt('Qancha pulingiz bor');
+
+// if (userMoney >= totalPriceInUzs) {
+//     console.log(`Sizning pulingiz yetarli ${userMoney - totalPriceInUzs} oshib ham qoladi`);
+// } else {
+//     console.log(`Pulingiz kam ekan atigi ${totalPriceInUzs - userMoney} yetmayapti`);
+// }
 
 
-// var myAge;
+// let myNum = Math.round(25.50);
 
-// console.log(typeof myAge);
-// var myAge = 22;
-// console.log(myAge);
+// console.log(myNum);
 
-// var education = null;
-// console.log(education);
+// const myNum = 6.8549545;
+// const fixedMyNum = myNum.toFixed(3);
 
-// var id = Symbol('5');
+// console.log(fixedMyNum);
 
-// var hugeNumber = BigInt(99566513516846521313213210);
+// const myRandomNum = Math.round(Math.random() * (100 - 70) + 70);
+
+// console.log(myRandomNum);
+
+// const myNum = Number(prompt('Raqam kiriting'))
+
+// console.log(typeof myNum);
+
+// const myNum = Number('5');
+
+// const myString = String(10);
+
+// const myBoolean = Boolean('');
+
+// console.log(typeof myNum);
+
+// console.log(typeof myString);
+
+// console.log(myBoolean);
+
+
+// const calcNum = 1 + 5 + '7';
+
+// console.log(calcNum);
+
+// const calcNum2 = 15 - '8';
+
+// console.log(calcNum2);
+
+// const calcNum = 15 + '2' - 3 + 4 - '15';
+
+// const myNums = '10' - '5';
+
+// console.log(calcNum);
+
+// console.log('23' + '10' - 3);
+
+// let n = 1 + '1';
+// n = n - 1;
+// console.log(n);
+
+// console.log(2 + 3 + 4 + '5');
+// console.log('10' - '4' - '3' - 2 + '5');
+
+// const myBoolean = Boolean(NaN);
+
+// console.log(myBoolean);
+
+
+// let money = '0';
+
+// if (money) {
+//     console.log('Pulingiz bor ekan');
+// } else {
+//     console.log('Pulingiz yoq ekan');
+// }
+
+
+
+// const height = 1.85;
+
+// if (height) {
+//     console.log('Bor');
+// } else {
+//     console.log('yoq');
+// }
+
+
+// const isDrivingLicense = true;
+// const isVisible = true;
+// const age = 15;
+// if (isDrivingLicense && isVisible || age >= 18) {
+//     console.log('Mashina hayday olasiz');
+// } else {
+//     console.log('Hayday olmaysiz');
+// }
+
+
+// const userAge = Number(prompt('Yoshingiz'));
+// const userGender = prompt('Iltimos jinsingiz');
+
+// if (userAge >= 18 && userGender === 'Male') {
+//     console.log('Siz saylovda qatnasha olasiz');
+// } else {
+//     console.log('Siz qatnasha olmaysiz');
+// // }
+
+// const myText = document.querySelectorAll('p');
+// console.log(myText);
+
+// const myMenu = document.getElementById('menu');
+
+// const myItem = document.getElementsByTagName('li');
+
+// console.log(myMenu);
+// console.log(myItem);
