78247c533fa123aaa00070b389d90010b59ee46e
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-03-02 14:27:34 +0500

    lesson-29

--- js/index.js
+++ js/index.js
@@ -1,30 +1,152 @@
+// const johnMassa = Number(prompt('Jonning massasi'));
+// const johnHeight = Number(prompt('Jonning height'));
+// const bobMassa = Number(prompt('Bobning massasi'));
+// const bobHeight = Number(prompt('Bobning height'));
 
-// var firstName = 'Muhammadrasul';
-// var age = 22;
+// const johnBmi = Math.round(johnMassa / johnHeight ** 2);
+// const bobBmi = Math.round(bobMassa / bobHeight ** 2);
 
-// console.log(typeof firstName);
-// console.log(typeof age);
-
-// var studentId = "123465";
-
-// console.log(typeof studentId);
-
-// var isStudent = true;
-// var isMarried = false;
-
-// console.log(typeof isStudent);
-// console.log(typeof isMarried);
+// if (johnBmi > bobBmi) {
+//     console.log(`Jonning BMI katta sababi ${johnBmi} chiqdi`);
+// } else if (johnBmi == bobBmi) {
+//     console.log('Ikkalasi ham teng');
+// } else {
+//     console.log(`Bobning BMI katta sababi ${bobBmi} chiqdi`);
+// }
 
 
-// var myAge;
+// const userAge = Math.round(Math.random() * 100);
 
-// console.log(typeof myAge);
-// var myAge = 22;
-// console.log(myAge);
+// console.log(userAge);
 
-// var education = null;
-// console.log(education);
+// if (userAge <= 18) {
+//     console.log('Siz o`qishingiz kerak');
+// } else if (userAge > 18 && userAge <= 60) {
+//     console.log('Siz ishlashingiz kerak');
+// } else {
+//     console.log('Siz pensiyadasiz');
+// }
+// const userAge = 22;
 
-// var id = Symbol('5');
+// switch (userAge) {
+//     case 18:
+//         console.log('Siz hali yoshsiz');
+//         break;
+//     case 22:
+//         console.log('Siz orta yoshsiz');
+//         break;
+//     case 25:
+//         console.log('Siz orta yoshsiz');
+//         break;
+//     default:
+//         console.log('Default qiymat');
+//         break;
+// }
 
-// var hugeNumber = BigInt(99566513516846521313213210);
+// const userAge = Number(prompt('Yoshingiz'));
+
+// switch (true) {
+//     case userAge <= 18:
+//         console.log('Siz hali yoshsiz');
+//         break;
+//     case userAge > 18 && userAge < 60:
+//         console.log('Siz ishlashingiz kerak');
+//         break;
+//     default:
+//         console.log('Siz pensiyadasiz');
+//         break;
+// }
+
+
+// const name = 'Muhammadrasul';
+
+// name === 'Muhammadrasul' ? console.log('Siz ustozsiz') : console.log('Siz boshqa odamsiz');
+
+// const myAge = 15;
+
+
+// const checkAge = myAge >= 18 ? 'Siz orta yoshsiz' : 'Siz yosh bolasiz';
+
+// console.log(checkAge);
+
+// userAge 18 dan baland bo'lsa va haydovchilik guvohnomasi bo'lsa siz mashina hayday olasiz aksi bo'lsa siz mashina hayday olmaysiz chiqsin.
+
+// const isDrivingLicence = true;
+// const userAge = Math.round(Math.random() * 22);
+// console.log(userAge);
+
+// const checkUser = isDrivingLicence && userAge >= 18 ? 'Siz mashina olsangiz boladi' : 'Siz ola bilmaysiz';
+
+// console.log(checkUser);
+
+
+
+// function myName(name, greeting) {
+//     return greeting + name;
+// }
+
+// console.log(myName('Muhammadrasul', 'salom'));
+// console.log(myName('Muhammad', 'qalaysiz'));
+// console.log(myName('Jasur', 'Uxalmayabsizmi'));
+
+
+
+// const userNum = +prompt('Raqam kiriting');
+
+// console.log(multiplyNum(userNum));
+
+
+// function multiplyNum(number) {
+//     return number * 2;
+// }
+
+// function makeJuice(apple, orange) {
+//     const juice = `Sok uchun bizga kerak ${apple} olma va ${orange} apelsin`;
+
+//     return juice;
+// }
+
+// console.log(makeJuice(5, 4));
+// console.log(makeJuice(2, 10));
+
+// function expression
+
+
+
+// const myFun = function (num) {
+//     return num * 2;
+// }
+
+// console.log(myFun(40));
+// console.log(myFun(10));
+
+// const funcWithArrow = (num) => {
+//     return num * 2;
+// }
+
+// console.log(funcWithArrow(5));
+// console.log(funcWithArrow(15));
+
+
+
+// const userAge1 = Number(prompt('Yosh kiriting'));
+// const userAge2 = Number(prompt('Yosh kiriting'));
+
+// function checkAge(number) {
+
+//     if (number >= 18) {
+//         return 'Siz katta yoshdasiz'
+//     } else {
+//         return 'Siz hali yoshsiz'
+//     }
+// }
+
+// console.log(checkAge(userAge1));
+// console.log(checkAge(userAge2));
+
+
+// function convertMinute(number) {
+//     return `${number * 60} sekund`;
+// }
+// console.log(convertMinute(25));
+// console.log(convertMinute(45));
