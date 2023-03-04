18fda7e339f9f064a27178f3b456748e2af30a01
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-03-04 14:25:12 +0500

    lesson-30

--- js/index.js
+++ js/index.js
@@ -1,30 +1,66 @@
+// function calcAverage(num1, num2, num3) {
+//     return (num1 + num2 + num3) / 3
+// }
 
-// var firstName = 'Muhammadrasul';
-// var age = 22;
-
-// console.log(typeof firstName);
-// console.log(typeof age);
-
-// var studentId = "123465";
-
-// console.log(typeof studentId);
-
-// var isStudent = true;
-// var isMarried = false;
-
-// console.log(typeof isStudent);
-// console.log(typeof isMarried);
+// const team1 = calcAverage(70, 80, 120);
+// const team2 = calcAverage(80, 70, 120);
 
 
-// var myAge;
+// const winnerFunction = (result1, result2) => {
+//     if (result1 > result2) {
+//         return 'Team1 yutdi'
+//     } else if (result1 === result2) {
+//         return 'Durrang'
+//     } else {
+//         return 'Team2 yutdi'
+//     }
+// }
 
-// console.log(typeof myAge);
-// var myAge = 22;
-// console.log(myAge);
+// console.log(winnerFunction(team1, team2));
 
-// var education = null;
-// console.log(education);
 
-// var id = Symbol('5');
+// function myFun(a, b = 0) {
+//     return a + b
+// }
 
-// var hugeNumber = BigInt(99566513516846521313213210);
+// console.log(myFun(4));
+
+// const myText = document.querySelector('.text');
+
+// // const newText = myText.textContent;
+// // const newTextWithInnerText = myText.innerText;
+
+// // console.log(newText);
+// // console.log(newTextWithInnerText);
+
+// myText.innerHTML = `<span>Hello World</span>`;
+
+// const btn = document.querySelector('.btn');
+// const myText = document.querySelector('.text');
+
+// const body = document.querySelector('body');
+
+// btn.addEventListener('click', function () {
+//     body.classList.toggle('dark')
+// })
+
+// const openBtn = document.querySelector('.open');
+// const closeBtn = document.querySelector('.close');
+// const modal = document.querySelector('.modal');
+
+
+// openBtn.addEventListener('click', () => {
+//     modal.classList.add('modal-open')
+// });
+
+// closeBtn.addEventListener('click', () => {
+//     modal.classList.remove('modal-open')
+// }); 
+
+function challengeFunction(num1, num2) {
+    return ((num1 >= 10 && num1 <= 20) !== (num2 >= 10 && num2 <= 20));
+}
+
+
+console.log(challengeFunction(10, 10));
+
