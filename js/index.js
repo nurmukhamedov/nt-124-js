c88080d2afa73e3b1c944b6661d9f38f316a6f88
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-03-28 10:09:47 +0500

    lesson-40

--- js/index.js
+++ js/index.js
@@ -1,30 +1,120 @@
+// const books = [
+//     { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925, genre: 'Fiction' }, { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960, genre: 'Fiction' }, { id: 3, title: '1984', author: 'George Orwell', year: 1949, genre: 'Science Fiction' }, { id: 4, title: 'Pride and Prejudice', author: 'Jane Austen', year: 1813, genre: 'Fiction' }, { id: 5, title: 'The Hobbit', author: 'J.R.R. Tolkien', year: 1937, genre: 'Fantasy' },];
 
-// var firstName = 'Muhammadrasul';
-// var age = 22;
+// const btn = document.querySelector('.btn');
+// const input = document.querySelector('.input');
+// const studentInfo = document.querySelector('.studentInfo');
 
-// console.log(typeof firstName);
-// console.log(typeof age);
+// btn.addEventListener('click', () => {
+//     const id = +input.value;
 
-// var studentId = "123465";
+//     const selectedBook = books.find((book) => book.id === id);
 
-// console.log(typeof studentId);
+//     if (selectedBook) {
+//         studentInfo.innerHTML = `<h2>Book: ${selectedBook.title} <span>Year: ${selectedBook.year}</span></h2> `
+//     } else {
+//         studentInfo.innerHTML = ` <h2>Not found</h2>`
+//     }
+// });
 
-// var isStudent = true;
-// var isMarried = false;
+// const list = document.querySelectorAll('.list li');
 
-// console.log(typeof isStudent);
-// console.log(typeof isMarried);
+// list.forEach((item) => {
+//     item.addEventListener('click', () => {
+//         console.log(`Clicked ${item.textContent}`);
+//     })
+// });
+
+// const person = {
+//     name: 'Muhammadrasul',
+//     age: 23
+// }
+// // console.log(person);
+// // console.log(JSON.stringify(person));
+
+// // const newPerson = JSON.stringify(person);
+
+// // const returnObj = JSON.parse(newPerson);
+
+// // console.log(returnObj);
+// // localStorage.setItem('info', newPerson);
+
+// // const firstName = localStorage.getItem('info');
+// // localStorage.removeItem('info')
+// // console.log(firstName);
+
+// const item2 = document.querySelector('.item2');
+// const item1 = item2.previousElementSibling;
+// const item3 = item2.nextElementSibling;
+
+// // console.log(item1);
+// // console.log(item3);
+
+// // const parentList = item1.parentNode;
+
+// // console.log(parentList);
 
 
-// var myAge;
+// const menu = item2.closest('body');
+// console.log(menu);
 
-// console.log(typeof myAge);
-// var myAge = 22;
-// console.log(myAge);
+const form = document.querySelector('.form'),
+    name = document.querySelector('#name'),
+    price = document.querySelector('#price'),
+    itemId = document.querySelector('#itemId'),
+    itemList = document.querySelector('.itemList');
 
-// var education = null;
-// console.log(education);
 
-// var id = Symbol('5');
+form.addEventListener('submit', (e) => {
 
-// var hugeNumber = BigInt(99566513516846521313213210);
+
+    e.preventDefault();
+
+    if (itemId.value) {
+
+        item = JSON.parse(localStorage.getItem(itemId.value));
+        item.name = name.value;
+        item.price = price.value
+
+    } else {
+
+        item = {
+            name: name.value,
+            price: price.value
+        }
+        itemId.value = Date.now();
+    }
+
+    localStorage.setItem(itemId.value, JSON.stringify(item));
+    form.reset();
+
+    render();
+})
+
+
+function render() {
+
+    itemList.innerHTML = ''
+
+    for (const key in localStorage) {
+        if (localStorage.hasOwnProperty(key)) {
+            const item = JSON.parse(localStorage.getItem(key))
+            const li = document.createElement('li');
+            li.innerHTML = `<span>Name: ${item.name}</span> <span>Price: $${item.price}</span>`;
+            itemList.appendChild(li);
+
+            const editButton = document.createElement('button');
+            editButton.textContent = 'Edit';
+            li.appendChild(editButton);
+
+            editButton.addEventListener('click', () => {
+                name.value = item.name;
+                price.value = item.price;
+                itemId.value = key;
+            })
+        }
+    }
+}
+
+render()
+
