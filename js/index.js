5278447fd3526ca39eb227696e3ca82b29416c08
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-03-30 10:29:26 +0500

    lesson-41

--- js/index.js
+++ js/index.js
@@ -1,120 +1,138 @@
-// const books = [
-//     { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925, genre: 'Fiction' }, { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960, genre: 'Fiction' }, { id: 3, title: '1984', author: 'George Orwell', year: 1949, genre: 'Science Fiction' }, { id: 4, title: 'Pride and Prejudice', author: 'Jane Austen', year: 1813, genre: 'Fiction' }, { id: 5, title: 'The Hobbit', author: 'J.R.R. Tolkien', year: 1937, genre: 'Fantasy' },];
+// const form = document.querySelector('.form'),
+//     name = document.querySelector('#name'),
+//     price = document.querySelector('#price'),
+//     itemId = document.querySelector('#itemId'),
+//     itemList = document.querySelector('.itemList');
 
-// const btn = document.querySelector('.btn');
-// const input = document.querySelector('.input');
-// const studentInfo = document.querySelector('.studentInfo');
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
 
-// btn.addEventListener('click', () => {
-//     const id = +input.value;
+//     if (itemId.value) {
 
-//     const selectedBook = books.find((book) => book.id === id);
+//         item = JSON.parse(localStorage.getItem(itemId.value));
+//         item.name = name.value;
+//         item.price = price.value
 
-//     if (selectedBook) {
-//         studentInfo.innerHTML = `<h2>Book: ${selectedBook.title} <span>Year: ${selectedBook.year}</span></h2> `
 //     } else {
-//         studentInfo.innerHTML = ` <h2>Not found</h2>`
+
+//         item = {
+//             name: name.value,
+//             price: price.value
+//         }
+//         itemId.value = Date.now();
 //     }
-// });
+//     localStorage.setItem(itemId.value, JSON.stringify(item));
+//     form.reset();
+//     itemId.value = '';
+//     render();
+// })
 
-// const list = document.querySelectorAll('.list li');
+// function render() {
+//     itemList.innerHTML = ''
 
-// list.forEach((item) => {
-//     item.addEventListener('click', () => {
-//         console.log(`Clicked ${item.textContent}`);
-//     })
-// });
+//     for (const key in localStorage) {
+//         if (localStorage.hasOwnProperty(key)) {
+//             const item = JSON.parse(localStorage.getItem(key))
+//             const li = document.createElement('li');
+//             li.innerHTML = `<span>Name: ${item.name}</span> <span>Price: $${item.price}</span>`;
+//             itemList.appendChild(li);
 
-// const person = {
-//     name: 'Muhammadrasul',
-//     age: 23
+//             const editButton = document.createElement('button');
+//             editButton.textContent = 'Edit';
+//             li.appendChild(editButton);
+
+//             editButton.addEventListener('click', () => {
+//                 name.value = item.name;
+//                 price.value = item.price;
+//                 itemId.value = key;
+//             })
+
+//             const deleteButton = document.createElement('button');
+//             deleteButton.textContent = 'Delete';
+//             li.appendChild(deleteButton);
+
+//             deleteButton.addEventListener('click', () => {
+//                 localStorage.removeItem(key);
+//                 render();
+//             });
+//         }
+//     }
 // }
-// // console.log(person);
-// // console.log(JSON.stringify(person));
 
-// // const newPerson = JSON.stringify(person);
-
-// // const returnObj = JSON.parse(newPerson);
-
-// // console.log(returnObj);
-// // localStorage.setItem('info', newPerson);
-
-// // const firstName = localStorage.getItem('info');
-// // localStorage.removeItem('info')
-// // console.log(firstName);
-
-// const item2 = document.querySelector('.item2');
-// const item1 = item2.previousElementSibling;
-// const item3 = item2.nextElementSibling;
-
-// // console.log(item1);
-// // console.log(item3);
-
-// // const parentList = item1.parentNode;
-
-// // console.log(parentList);
+// render();
 
 
-// const menu = item2.closest('body');
-// console.log(menu);
+// const box = document.querySelector('.box');
 
-const form = document.querySelector('.form'),
-    name = document.querySelector('#name'),
-    price = document.querySelector('#price'),
-    itemId = document.querySelector('#itemId'),
-    itemList = document.querySelector('.itemList');
+// const text1 = document.createElement('p')
+// const text2 = document.createElement('p')
+// const text3 = document.createElement('p')
+
+// const myFragment = document.createDocumentFragment();
+
+// myFragment.appendChild(text1);
+// myFragment.appendChild(text2);
+// myFragment.appendChild(text3);
+
+// box.appendChild(myFragment);
+// const myFunc = () => {
+//     console.log('Async code');
+// }
+
+// setTimeout(myFunc, 2000);
+
+// console.log('Hello World');
 
 
-form.addEventListener('submit', (e) => {
+// console.log('By World');
 
 
-    e.preventDefault();
+// let pizza;
 
-    if (itemId.value) {
+// function orderPizza() {
+//     console.log('Order pizza');
 
-        item = JSON.parse(localStorage.getItem(itemId.value));
-        item.name = name.value;
-        item.price = price.value
+//     setTimeout(() => {
+//         pizza = 'Pizza'
+//     }, 2000);
+// }
+// orderPizza();
+// console.log('Call my friend');
 
+// console.log('Having a shower');
+
+// setTimeout(() => {
+//     console.log(`Eating ${pizza}`);
+// }, 2500);
+
+const box = document.querySelector('.box');
+
+fetch('https://restcountries.com/v3.1/all').then((response) => response.json()).then((data) => data.map((countries) => {
+    const img = document.createElement('img');
+    img.src = `${countries.flags.png}`
+    img.alt = `"${countries.flags.alt}"`
+
+    box.appendChild(img);
+
+    const name = document.createElement('h2');
+    name.textContent = countries.name.common;
+
+    box.appendChild(name);
+
+    const capital = document.createElement('h4');
+    capital.textContent = countries.capital
+
+    box.appendChild(capital);
+
+    const populationNum = document.createElement('h4');
+
+    const populationInString = String(countries.population);
+
+    if (populationInString.length === 6) {
+        populationNum.textContent = `${populationInString.slice(0, 3)}k`;
+        box.appendChild(populationNum)
     } else {
-
-        item = {
-            name: name.value,
-            price: price.value
-        }
-        itemId.value = Date.now();
+        populationNum.textContent = populationInString;
+        box.appendChild(populationNum);
     }
-
-    localStorage.setItem(itemId.value, JSON.stringify(item));
-    form.reset();
-
-    render();
-})
-
-
-function render() {
-
-    itemList.innerHTML = ''
-
-    for (const key in localStorage) {
-        if (localStorage.hasOwnProperty(key)) {
-            const item = JSON.parse(localStorage.getItem(key))
-            const li = document.createElement('li');
-            li.innerHTML = `<span>Name: ${item.name}</span> <span>Price: $${item.price}</span>`;
-            itemList.appendChild(li);
-
-            const editButton = document.createElement('button');
-            editButton.textContent = 'Edit';
-            li.appendChild(editButton);
-
-            editButton.addEventListener('click', () => {
-                name.value = item.name;
-                price.value = item.price;
-                itemId.value = key;
-            })
-        }
-    }
-}
-
-render()
-
+}))
