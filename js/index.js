d012c242e07a618d5d197a9bd496b8dac4aa2345
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-04-13 10:04:07 +0500

    lesson-47

--- js/index.js
+++ js/index.js
@@ -1,30 +1,121 @@
+// const person = {
+//     name: 'Muhammadrasul',
+//     age: 23,
+//     info: function () {
+//         console.log(`My name is ${this.name} and my age ${this.age}`);
+//     }
+// }
+// const person2 = {
+//     name: 'John',
+//     age: 32,
+//     info: person.info
+// }
 
-// var firstName = 'Muhammadrasul';
-// var age = 22;
-
-// console.log(typeof firstName);
-// console.log(typeof age);
-
-// var studentId = "123465";
-
-// console.log(typeof studentId);
-
-// var isStudent = true;
-// var isMarried = false;
-
-// console.log(typeof isStudent);
-// console.log(typeof isMarried);
+// person2.info();
 
 
-// var myAge;
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     sayHello() {
+//         return `Hello my name is ${this.name} and my age is ${this.age}`
+//     }
+// }
 
-// console.log(typeof myAge);
-// var myAge = 22;
-// console.log(myAge);
+// const teacher = new Person('Muhammadrasul', 23);
+// const student = new Person('John', 35);
+// console.log(teacher.sayHello());
+// console.log(student);
 
-// var education = null;
-// console.log(education);
+// function People(name, age) {
 
-// var id = Symbol('5');
+//     this.name = name;
+//     this.age = age;
 
-// var hugeNumber = BigInt(99566513516846521313213210);
+//     this.info = function () {
+//         return `Hello my name is ${this.name} and my age is ${this.age}`
+//     }
+// }
+
+// const student2 = new People('Jack', 34)
+// console.log(student2);
+
+
+
+// class Person {
+//     constructor(name, age) {
+//         let _name = name;
+//         let _age = age;
+
+//         this.getName = function () {
+//             return _name;
+//         }
+//         this.getAge = function () {
+//             return _age;
+//         }
+//     }
+// }
+
+// const student3 = new Person('John', 24)
+
+// console.log(student3.getAge());
+
+
+// class Animal {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     speak() {
+//         console.log(`${this.name} shovqinli`);
+//     }
+// }
+
+// class Cat extends Animal {
+//     constructor(name, age) {
+//         super(name, age)
+//     }
+
+//     speak() {
+//         console.log(`${this.name} meow`);
+//     }
+// }
+
+// const newAnimal = new Animal('Reks', 3);
+// console.log(newAnimal);
+// newAnimal.speak();
+
+// const cat = new Cat('Kiki', 1);
+
+// console.log(cat);
+
+// cat.speak();  
+
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 2,
+    spaceBetween: 30,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
